0990f6a4e5533ed539feda12d9ad3d583c5b89f6
jiyaohui <jiyaohui@gmail.com>
2022-04-28 17:25:20 +0800

    path_config.py加载预训练模型

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -2,6 +2,8 @@
 e4e = './pretrained_models/e4e_ffhq_encode.pt'
 stylegan2_ada_ffhq = 'pretrained_models/ffhq.pkl'
 ir_se50 = './pretrained_models/model_ir_se50.pth'
+shape_predictor_path = 'pretrained_models/shape_predictor_68_face_landmarks.dat'
+segmentation_model_path = 'pretrained_models/79999_iter.pth'
 
 ## Dirs for output files
 checkpoints_dir = './checkpoints'
@@ -14,8 +16,3 @@ interfacegan_folder = 'editings/w_directions/'
 styleclip_fs3 = 'editings/styleclip_global/fs3.npy'
 
 stylespace_mean_std = 'editings/styleclip_global/S_mean_std'
-
-shape_predictor_path = 'pretrained_models/shape_predictor_68_face_landmarks.dat'
-
-segmentation_model_path = 'pretrained_models/79999_iter.pth'
-
